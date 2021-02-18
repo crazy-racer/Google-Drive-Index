c9809ff356f470853c0cef16b5cf62ee7cd7ef7d
yamraaj007 <69627019+crazy-racer@users.noreply.github.com>
2021-02-19 02:08:30 +0530

    Update worker-beta.js

--- worker/worker-beta.js
+++ worker/worker-beta.js
@@ -1,5 +1,5 @@
 /**
-* A Script Redesigned by Parveen Bhadoo from GOIndex at https://github.com/ParveenBhadooOfficial/Bhadoo-Drive-Index
+* A Script Redesigned by Mirror∆Cloud from GOIndex at https://github.com/crazy-racer/Bhadoo-Drive-Index
 */
 const authConfig = {
 	"siteName": "Bhadoo Drive Index", // Website name
@@ -56,15 +56,15 @@ const authConfig = {
  */
 const uiConfig = {
 	"theme": "bhadoo_bootstrap", // Change doesn't works
-	"dark_mode": true, // switch between light or dark themes
+	"dark_mode": false, // switch between light or dark themes
 	"version": "2.0.7", // don't touch this one. get latest code using generator at https://github.com/ParveenBhadooOfficial/Bhadoo-Drive-Index
-	"logo_image": false, // true if you're using image link in next option.
-	"logo_height": "", // only if logo_image is true
-	"logo_width": "100px", // only if logo_image is true
-	"logo_link_name": "Bhadoo Drive Index", // if logo is true then link otherwise just text for name
+	"logo_image": true, // true if you're using image link in next option.
+	"logo_height": "99px", // only if logo_image is true
+	"logo_width": "710px", // only if logo_image is true
+	"logo_link_name": "<img src"images/yamraaj.png", // if logo is true then link otherwise just text for name
 	"contact_link": "https://t.telegram.ind.in/BhadooCloud", // Link to Contact Button on Menu
 	"copyright_year": "2050", // year of copyright, can be anything like 2015 - 2020 or just 2020
-	"company_name": "Bhadoo Cloud", // Name next to copyright
+	"company_name": "yamraaj007", // Name next to copyright
 	"company_link": "https://t.telegram.ind.in/BhadooCloud", // link of copyright name
 	"credit": true, // Set this to true to give us credit
 	"display_size": true, // Set this to false to hide display file size
